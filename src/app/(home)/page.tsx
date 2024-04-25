fb88c56ed1eb2b2f0feafec22b87b0f6a2317870
fadlystwn <fadlystwn@gmail.com>
2024-04-25 11:32:12 +0700

    Refactor file paths, update fetchData utility function, and remove unused page component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,27 @@
 import Link from 'next/link';
-import CardList from '@/components/CardList';
+import Card from '@/components/Card';
 import { fetchData } from '@/utils/fetchData';
+import { User } from '@/types/User';
 
-export default async function User() {
+export default async function Page() {
   const data = await fetchData('https://jsonplaceholder.typicode.com/users')
 
   return (
-    <div>
+    <div data-testid="user">
       <h1 className="text-2xl">Home</h1>
-      <CardList items={data} />
+      return (
+      <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
+        {data && data.map((item: User) => (
+          <Link key={item.id} href={`/users/${item.id}`}>
+            <Card key={item.id}
+              name={item.name}
+              company={item.company.name}
+              username={item.username}
+            />
+          </Link>
+        ))}
+      </div>
+      );
     </div>
   )
 }
